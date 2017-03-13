02840229f5bfb6f75f6697286b3ee78f379a1bae
Aleksander Grzybowski <kelog@orange.pl>
2017-03-13 21:23:32 +0100

    Remove unnecessary (they say) interface

--- src/main/java/org/kelog/todolist/service/TodoService.java
+++ src/main/java/org/kelog/todolist/service/TodoService.java
@@ -1,18 +1,54 @@
 package org.kelog.todolist.service;
 
+import org.kelog.todolist.dao.TodoRepository;
 import org.kelog.todolist.model.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.Optional;
 
-public interface TodoService {
-    Collection<Todo> findAll();
+@Component
+public class TodoService {
     
-    Optional<Todo> findById(int id);
+    private TodoRepository repository;
     
-    Todo create(String description, Boolean important);
+    @Autowired
+    public TodoService(TodoRepository repository) {
+        this.repository = repository;
+    }
     
-    Todo update(int id, String description, Boolean importance) throws TodoNotFoundException;
+    public Collection<Todo> findAll() {
+        return repository.findAll();
+    }
     
-    void delete(int id) throws TodoNotFoundException;
+    public Optional<Todo> findById(int id) {
+        return repository.findById(id);
+    }
+    
+    public Todo create(String description, Boolean important) {
+        return repository.save(new Todo(description, important));
+    }
+    
+    public Todo update(int id, String description, Boolean importance) {
+        Todo item = findOrThrow(id);
+        
+        if (description != null) {
+            item.setDescription(description);
+        }
+        
+        if (importance != null) {
+            item.setImportant(importance);
+        }
+        
+        return repository.save(item);
+    }
+    
+    public void delete(int id) {
+        repository.delete(findOrThrow(id));
+    }
+    
+    private Todo findOrThrow(int id) {
+        return repository.findById(id).orElseThrow(TodoNotFoundException::new);
+    }
 }
